1a58e8f77ea21f77feadccc7cdcf3c01ba1747b0
Normirzaev Abdurahmon <anormirzaev5>
2022-01-14 20:10:09 +0500

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
+
+import Header from './components/header/Header';
+
+
+import './App.css';
+
 function App() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <div className="Main">
+            <Header/>
+            <div className="Main_container">
+                <div className="Container">
+                    <div className="Con_flex">
+                    <div className="Content_tx">
+                        <div className="con_tx">
+                            <h1>SiteLabs</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco</p>
+                        </div>
+                    </div>
+
+                    <div className="Content_video">
+                        <div className="Video_img">
+                            <img src={"./img/play_btn.png"} alt="photo"/>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
