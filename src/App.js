253094f5dd40397501336c284f8947992b28124b
Abdurahmon <anormirzaev5@gmail.com>
2022-01-15 14:47:18 +0500

    add home

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import './App.css';
+import Home from "./components/home/Home";
 
 function App() {
     return (
         <div className="Main">
             <Header/>
             <div className="Main_container">
-                <div className="Container">
-                    <div className="Con_flex">
-                        <div className="Content_tx">
-                            <div className="con_tx">
-                                <h1>SiteLabs</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco</p>
-                            </div>
-                        </div>
-                        <div className="Content_video">
-                            <div className="Video_img">
-                                <img src={"./img/play_btn.png"} alt="photo"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Home/>
             </div>
         </div>
     );
---
+++ src/components/home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="Container">
+            <div className="Con_flex">
+                <div className="Content_tx">
+                    <div className="con_tx">
+                        <h1>SiteLabs</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco</p>
+                    </div>
+                </div>
+                <div className="Content_video">
+                    <div className="Video_img">
+                        <img src={"./img/play_btn.png"} alt="photo"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Home
