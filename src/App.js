9eb5e980c1bbc897be36c80c86c3dce0401e024a
Hamidov Muhammadjon <muhammadjon202>
2022-01-15 14:43:30 +0500

    Added and deleted files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-
-
-import Header from './components/header/Header';
-
-
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
@@ -13,20 +9,19 @@ function App() {
             <div className="Main_container">
                 <div className="Container">
                     <div className="Con_flex">
-                    <div className="Content_tx">
-                        <div className="con_tx">
-                            <h1>SiteLabs</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                ullamco</p>
+                        <div className="Content_tx">
+                            <div className="con_tx">
+                                <h1>SiteLabs</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco</p>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="Content_video">
-                        <div className="Video_img">
-                            <img src={"./img/play_btn.png"} alt="photo"/>
+                        <div className="Content_video">
+                            <div className="Video_img">
+                                <img src={"./img/play_btn.png"} alt="photo"/>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header_container">
+                <div className="con_logo">
+                    <img src={"../../img/site.lab.png"} alt="site.lab"/>
+                </div>
+                <div className="con_nav_bar">
+                    <ul className="ul">
+                        <li className="list_item"><a href="#">Home</a></li>
+                        <li className="list_item"><a href="#">About</a></li>
+                        <li className="list_item"><a href="#">Contact</a></li>
+                        <li className="bt"><a href="#">Get Started</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
