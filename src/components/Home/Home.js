954e1a262e7257852f31c0ab7506f7cfa4640e43
Hamidov Muhammadjon <muhammadjon202>
2022-01-17 17:02:04 +0500

    Added and deleted files

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,16 +11,21 @@ function Home() {
                         <div className="Con_flex">
                             <div className="Content_tx">
                                 <div className="con_tx">
-                                    <h1>SiteLabs</h1>
+                                    <h1>Site Labs Technology</h1>
                                     <p className="con_tx-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                                         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                                         nostrud
-                                        exercitation ullamco</p>
+                                        exercitation ullamco
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
                                 </div>
                             </div>
                             <div className="Content_video">
                                 <div className="Video_img">
-                                    <img src={"./img/play_btn.png"} alt="photo"/>
+                                    {/*<img src={"./img/play_btn.png"} alt="photo"/>*/}
+                                    <div className="play">
+                                        <i className="fas fa-play"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
