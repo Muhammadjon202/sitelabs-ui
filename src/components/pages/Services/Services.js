f684df71b4f9ec872ddd61c1c89154cfeffc2752
Najmiddin Muhitdinov <najmiddin0107>
2022-01-31 15:25:21 +0500

    Servoces page

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -18,7 +18,8 @@ function Services() {
                         <p>OUR SERVICES</p>
                         <div className="services_container_text_description">
                             <h2>Solutions & Focus Areas</h2>
-                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br/> incididunt ut labore et dolore magna aliqua</h5>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor<br/> incididunt ut labore et dolore magna aliqua</h5>
                         </div>
                     </div>
                     <div className="services_categories">
@@ -28,71 +29,111 @@ function Services() {
                                 <img src={"img/icons/strategy.png"} width={"75px"} alt=""/>
                                 <h2>Marketing Strategy</h2>
                                 <p>
-                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore</p>
-                           <div className="services_read_category">
-                               <h5>Read More </h5>
-                               <img src={"img/icons/right-row.png"} alt=""/>
-                           </div>
-
-                            </div>
-                            <div className="services_it_category first_categories">
-                                <img src={"img/icons/document.png"} width={"75px"} alt=""/>
-                                <h2>IT Management</h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore</p>
+                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore dolore</p>
                                 <div className="services_read_category">
-                                    <h5 className={"for_it"}>Read More </h5>
+                                    <h5>Read More </h5>
                                     <img src={"img/icons/right-row.png"} alt=""/>
                                 </div>
+
                             </div>
-                            <div className="services_cyber_category first_categories">
-                                <img src={"img/icons/cyber.png"} width={"75px"} alt=""/>
-                                <h2>Cyber Security</h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore</p>
-                                <div className="services_read_category">
-                                    <h5 >Read More </h5>
-                                    <img src={"img/icons/right-row.png"} alt=""/>
+                            <div className="it_cyber">
+                                <div className="services_it_category first_categories oth">
+                                    <img src={"img/icons/document.png"} width={"75px"} alt=""/>
+                                    <h2>IT Management</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore dolore</p>
+                                    <div className="services_read_category">
+                                        <h5 className={"for_it"}>Read More </h5>
+                                        <img src={"img/icons/right-row.png"} alt=""/>
+                                    </div>
+                                </div>
+                                <div className="services_cyber_category first_categories oth">
+                                    <img src={"img/icons/cyber.png"} width={"75px"} alt=""/>
+                                    <h2>Cyber Security</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore dolore</p>
+                                    <div className="services_read_category">
+                                        <h5>Read More </h5>
+                                        <img src={"img/icons/right-row.png"} alt=""/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div className="services_categories_second">
-                            <div className="services_marketing_category second_categories">
+                            <div className="services_cloud_category second_categories">
 
                                 <img src={"img/icons/cloud.png"} width={"75px"} alt=""/>
                                 <h2>Cloud Computing</h2>
                                 <p>
-                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore</p>
+                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore dolore</p>
                                 <div className="services_read_category">
                                     <h5>Read More </h5>
                                     <img src={"img/icons/right-row.png"} alt=""/>
                                 </div>
 
                             </div>
-                            <div className="services_soft_category second_categories">
-                                <img src={"img/icons/computer.png"} width={"75px"} alt=""/>
-                                <h2>Software Developer</h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore</p>
-                                <div className="services_read_category">
-                                    <h5>Read More </h5>
-                                    <img src={"img/icons/right-row.png"} alt=""/>
+                            <div className="soft_consult">
+                                <div className="services_soft_category second_categories oth">
+                                    <img src={"img/icons/computer.png"} width={"75px"} alt=""/>
+                                    <h2>Software Developer</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore dolore</p>
+                                    <div className="services_read_category">
+                                        <h5>Read More </h5>
+                                        <img src={"img/icons/right-row.png"} alt=""/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="services_cyber_category second_categories">
-                                <img src={"img/icons/consult.png"} width={"75px"} alt=""/>
-                                <h2>IT Consultancy </h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore</p>
-                                <div className="services_read_category">
-                                    <h5 >Read More </h5>
-                                    <img src={"img/icons/right-row.png"} alt=""/>
+                                <div className="services_consult_category second_categories oth">
+                                    <img src={"img/icons/consult.png"} width={"75px"} alt=""/>
+                                    <h2>IT Consultancy </h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore dolore</p>
+                                    <div className="services_read_category">
+                                        <h5>Read More </h5>
+                                        <img src={"img/icons/right-row.png"} alt=""/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="services_clients">
+                <div className="left">
 
+                </div>
+                <div className="right">
+                    <h2>What Client Say<br/> About Us</h2>
+                    <img src={"/img/Services/quote.png"} width={"80px"} alt=""/>
+                    <p>Vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti atque corrupti quos dolores et quas molestias excepturi sint</p>
+                    <div className="person_img">
+                        <img src={"/img/services/person.png"} alt="" width={"70px"}/>
+
+                    </div>
+                    <h3>Harley Burch</h3>
+                    <p>Entrepreneur</p>
+                </div>
+            </div>
+            <div className="services_plans">
+                <div className="services_plans_text">
+                    <p>OUR PRICE</p>
+                    <h2>Choose Your Plan</h2>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br/> incididunt
+                        ut
+                        labore et dolore magna aliqua </h5>
+                </div>
+                <div className="services_plans_content">
+                    <div className="basic plans"> </div>
+                    <div className="silver plans"> </div>
+                    <div className="gold plans"> </div>
+                </div>
             </div>
         </div>
     );
